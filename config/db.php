ee14f8d0854bc8058d74f15e1b557c5a07ae7999
picnii <sompop@picnii.me>
2015-08-17 21:35:00 +0700

    ignore db update

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=invoice_bill_app',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
